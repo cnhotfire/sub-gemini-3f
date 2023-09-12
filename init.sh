fba409e3c2d477153d14b4e428a2be4dc1af8ecb
cnhotfire <34259220+cnhotfire@users.noreply.github.com>
2023-09-12 18:36:21 +0800

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# update ubuntu-x86_64-v2-gemini-3f-2023-sep-11
+apt install libgomp1 -y
+wget https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-11/subspace-node-ubuntu-x86_64-v2-gemini-3f-2023-sep-11
+mv subspace-node-ubuntu-x86_64-v2-gemini-3f-2023-sep-11 node
+wget https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-11/subspace-farmer-ubuntu-x86_64-v2-gemini-3f-2023-sep-11
+mv subspace-farmer-ubuntu-x86_64-v2-gemini-3f-2023-sep-11 farmer
+chmod +x node.sh farmer.sh node farmer
+mkdir /data/subspace/node-data
+mkdir /data/subspace/farmer-data
