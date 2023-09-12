05f00bcb4cc8e8268f62e9eb2efd5cde0524cdb8
cnhotfire <34259220+cnhotfire@users.noreply.github.com>
2023-09-12 18:43:13 +0800

    Add files via upload

---
+++ node.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 例子：nohup ./node \
+#   --chain gemini-3f \
+#   --execution wasm \
+#   --blocks-pruning 256 \
+#   --state-pruning archive \
+#   --no-private-ipv4 \
+#   --validator \
+# --name syskey \
+# > node.log 2>&1 &
+
+# 将INSERT_YOUR_ID修改为自己要运行的节点的名字
+nohup ./node \
+  --base-path "/data/subspace/node-data" \
+  --chain gemini-3f \
+  --execution wasm \
+  --blocks-pruning 256 \
+  --state-pruning archive \
+  --no-private-ipv4 \
+  --validator \
+  --name "" \
+  >> node.log 2>&1 &
