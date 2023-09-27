8ec6b154a5c1ef776361d1a5959d3dcb8ac9b220
cnhotfire <34259220+cnhotfire@users.noreply.github.com>
2023-09-27 19:50:09 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ mv subspace-node-ubuntu-x86_64-v2-gemini-3f-2023-sep-11 node
 wget https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-11/subspace-farmer-ubuntu-x86_64-v2-gemini-3f-2023-sep-11
 mv subspace-farmer-ubuntu-x86_64-v2-gemini-3f-2023-sep-11 farmer
 chmod +x node.sh farmer.sh node farmer
-mkdir /data/subspace/node-data
-mkdir /data/subspace/farmer-data
+mkdir /data2/subspace/node-data
+mkdir /data2/subspace/farmer-data
